7161085efbf35f72a5aceb9fc57bab7cd1a03e0e
Afanasiev Dmitriy <59284294+d-afanasiev@users.noreply.github.com>
2021-04-28 16:19:09 +0300

    hw-35

--- index.js
+++ index.js
@@ -1,35 +1,20 @@
-// // Массив чисел будет отсортирован по возврастанию
 // const scores = [61, 19, 74, 35, 92, 56];
-// scores.sort();
-// console.log(scores); // [19, 35, 56, 61, 74, 92]
-
-// // Массив строк сортируется по алфавиту
-// const students = ["Вика", "Андрей", "Олег", "Юля", "Борис", "Катя"];
-// students.sort();
-// console.log(students); // [ 'Андрей', 'Борис', 'Вика', 'Катя', 'Олег', 'Юля' ]
-
-// // При этом порядковый номер заглавных букв меньше чем у прописных
-// const letters = ["b", "B", "a", "A", "c", "C"];
-// letters.sort();
-// console.log(letters); // ['A', 'B', 'C', 'a', 'b', 'c']
-
-// const scores = [61, 19, 74, 35, 92, 56];
-// const ascendingScores = [...scores].sort();
-
-// console.log(scores); // [61, 19, 74, 35, 92, 56]
+// const ascendingScores = [...scores].sort((a, b) => a - b);
 // console.log(ascendingScores); // [19, 35, 56, 61, 74, 92]
 
+// const scores = [61, 19, 74, 35, 92, 56];
+// const descendingScores = [...scores].sort((a, b) => b - a);
+// console.log(descendingScores); // [92, 74, 61, 56, 35, 19]
+
 const releaseDates = [2016, 1967, 2008, 1984, 1973, 2012, 1997];
-const authors = [
-  "Ли Танит",
-  "Бернард Корнуэлл",
-  "Роберт Шекли",
-  "Федор Достоевский",
-];
 // Пиши код ниже этой строки
 
-const ascendingReleaseDates = [...releaseDates].sort();
+const ascendingReleaseDates = [...releaseDates].sort(
+  (firsEl, lastEl) => firsEl - lastEl
+);
 
-const alphabeticalAuthors = [...authors].sort();
+const descendingReleaseDates = [...releaseDates].sort(
+  (firsEl, lastEl) => lastEl - firsEl
+);
 
-console.log(ascendingReleaseDates, alphabeticalAuthors);
+console.log(ascendingReleaseDates, descendingReleaseDates);
