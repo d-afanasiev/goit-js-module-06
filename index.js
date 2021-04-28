a42ed34eb2325f15243090928f286d08e390c520
Afanasiev Dmitriy <59284294+d-afanasiev@users.noreply.github.com>
2021-04-28 11:13:37 +0300

    hw-03

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
-function filterArray(numbers, value) {
-  const filteredNumbers = [];
+function getCommonElements(firstArray, secondArray) {
+  const commonElements = [];
   // Пиши код ниже этой строки
 
-  // for (let i = 0; i < numbers.length; i += 1) {
-  //   if (numbers[i] > value) {
-  //     filteredNumbers.push(numbers[i]);
+  // for (let i = 0; i < firstArray.length; i += 1) {
+  //   if (secondArray.includes(firstArray[i])) {
+  //     commonElements.push(firstArray[i]);
   //   }
   // }
 
-  numbers.forEach((element) => {
-    if (element > value) {
-      filteredNumbers.push(element);
+  firstArray.forEach((element) => {
+    if (secondArray.includes(element)) {
+      commonElements.push(element);
     }
   });
 
+  console.log(commonElements);
+  return commonElements;
   // Пиши код выше этой строки
-  console.log(filteredNumbers);
-  return filteredNumbers;
 }
 
-filterArray([1, 2, 3, 4, 5], 3);
+getCommonElements([24, 12, 27, 3], [12, 8, 3, 36, 27]);
