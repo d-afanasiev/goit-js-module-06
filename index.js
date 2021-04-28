ee36cbd5c2cfeceab2d3b13d1a07b803958ca9a2
Afanasiev Dmitriy <59284294+d-afanasiev@users.noreply.github.com>
2021-04-28 11:21:53 +0300

    hw-06

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
-// // До
-// function classicAdd(a, b, c) {
-//   return a + b + c;
-// }
+// const numbers = [5, 10, 15, 20, 25];
 
-// // После
-// const arrowAdd = (a, b, c) => a + b + c;
+// // Объявление функции
+// numbers.forEach(function (number, index) {
+//   console.log(`Индекс ${index}, значение ${number}`);
+// });
+
+// // Анонимная стрелочная функция
+// numbers.forEach((number, index) => {
+//   console.log(`Индекс ${index}, значение ${number}`);
+// });
+
+// const numbers = [5, 10, 15, 20, 25];
+
+// const logMessage = (number, index) => {
+//   console.log(`Индекс ${index}, значение ${number}`);
+// };
+
+// numbers.forEach(logMessage);
 
 // Пиши код ниже этой строки
-const calculateTotalPrice = (quantity, pricePerItem) => quantity * pricePerItem;
+const calculateTotalPrice = (orderedItems) => {
+  let totalPrice = 0;
+
+  orderedItems.forEach((item) => {
+    totalPrice += item;
+  });
+
+  console.log(totalPrice);
+  return totalPrice;
+};
 // Пиши код выше этой строки
+
+calculateTotalPrice([164, 48, 291]);
