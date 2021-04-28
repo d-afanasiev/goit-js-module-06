fe6a6848ab25f3db6a81d150b43e256985df0bdc
Afanasiev Dmitriy <59284294+d-afanasiev@users.noreply.github.com>
2021-04-28 11:18:01 +0300

    hw-05

--- index.js
+++ index.js
@@ -1,16 +1,11 @@
-// // Обычное объявление функции
+// // До
 // function classicAdd(a, b, c) {
 //   return a + b + c;
 // }
 
-// // Тоже самое как стрелочная функция
-// const arrowAdd = (a, b, c) => {
-//   return a + b + c;
-// };
+// // После
+// const arrowAdd = (a, b, c) => a + b + c;
 
 // Пиши код ниже этой строки
-
-const calculateTotalPrice = (quantity, pricePerItem) => {
-  // Пиши код выше этой строки
-  return quantity * pricePerItem;
-};
+const calculateTotalPrice = (quantity, pricePerItem) => quantity * pricePerItem;
+// Пиши код выше этой строки
