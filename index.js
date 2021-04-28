024d5bded9499d3ea4b091a970388d7d77c618f6
Afanasiev Dmitriy <59284294+d-afanasiev@users.noreply.github.com>
2021-04-28 11:09:37 +0300

    hw-02

--- index.js
+++ index.js
@@ -1,30 +1,22 @@
-// const numbers = [5, 10, 15, 20, 25];
-
-// // Классический for
-// for (let i = 0; i < numbers.length; i += 1) {
-//   console.log(`Индекс ${i}, значение ${numbers[i]}`);
-// }
-
-// // Перебирающий forEach
-// numbers.forEach(function (number, index) {
-//   console.log(`Индекс ${index}, значение ${number}`);
-// });
-
-function calculateTotalPrice(orderedItems) {
-  let totalPrice = 0;
+function filterArray(numbers, value) {
+  const filteredNumbers = [];
   // Пиши код ниже этой строки
 
-  // for (let i = 0; i < orderedItems.length; i += 1) {
-  //   totalPrice += orderedItems[i];
+  // for (let i = 0; i < numbers.length; i += 1) {
+  //   if (numbers[i] > value) {
+  //     filteredNumbers.push(numbers[i]);
+  //   }
   // }
 
-  orderedItems.forEach((element) => {
-    totalPrice += element;
+  numbers.forEach((element) => {
+    if (element > value) {
+      filteredNumbers.push(element);
+    }
   });
 
   // Пиши код выше этой строки
-  console.log(totalPrice);
-  return totalPrice;
+  console.log(filteredNumbers);
+  return filteredNumbers;
 }
 
-calculateTotalPrice([12, 85, 37, 4]);
+filterArray([1, 2, 3, 4, 5], 3);
