824414931f8da0b5c00448a08632017a49199a8f
Afanasiev Dmitriy <59284294+d-afanasiev@users.noreply.github.com>
2021-04-28 12:49:59 +0300

    hw-11

--- index.js
+++ index.js
@@ -1,16 +1,23 @@
-// const planets = ["Земля", "Марс", "Венера", "Юпитер"];
+// const students = [
+//   { name: "Манго", score: 83 },
+//   { name: "Поли", score: 59 },
+//   { name: "Аякс", score: 37 },
+//   { name: "Киви", score: 94 },
+//   { name: "Хьюстон", score: 64 },
+// ];
 
-// const planetsInUpperCase = planets.map((planet) => planet.toUpperCase());
-// console.log(planetsInUpperCase); // ['ЗЕМЛЯ', 'МАРС', 'ВЕНЕРА', 'ЮПИТЕР']
+// const names = students.map((student) => student.name);
+// console.log(names); // ['Манго', 'Поли', 'Аякс', 'Киви', 'Хьюстон']
 
-// const planetsInLowerCase = planets.map((planet) => planet.toLowerCase());
-// console.log(planetsInLowerCase); // ['земля', 'марс', 'венера', 'юпитер']
-
-// // Оригинальный массив не изменился
-// console.log(planets); // ['Земля', 'Марс', 'Венера', 'Юпитер']
-
-const planets = ["Земля", "Марс", "Венера", "Юпитер"];
+const books = [
+  { title: "Последнее королевство", author: "Бернард Корнуэлл", rating: 8.38 },
+  { title: "На берегу спокойных вод", author: "Роберт Шекли", rating: 8.51 },
+  { title: "Сон смешного человека", author: "Федор Достоевский", rating: 7.75 },
+  { title: "Красна как кровь", author: "Ли Танит", rating: 7.94 },
+  { title: "Враг Божий", author: "Бернард Корнуэлл", rating: 8.67 },
+];
 // Пиши код ниже этой строки
 
-const planetsLengths = planets.map((planet) => planet.length);
-console.log(planetsLengths);
+const titles = books.map((book) => book.title);
+
+console.log(titles);
