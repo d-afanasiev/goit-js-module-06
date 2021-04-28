2751218666c5b7efc6ac36de68951701cea38de8
Afanasiev Dmitriy <59284294+d-afanasiev@users.noreply.github.com>
2021-04-28 12:44:48 +0300

    hw-10

--- index.js
+++ index.js
@@ -1,57 +1,16 @@
-// Функция с побочными эффектами
-//
-// const dirtyMultiply = (array, value) => {
-//   for (let i = 0; i < array.length; i += 1) {
-//     array[i] = array[i] * value;
-//   }
-// };
+// const planets = ["Земля", "Марс", "Венера", "Юпитер"];
 
-// const numbers = [1, 2, 3, 4, 5];
-// dirtyMultiply(numbers, 2);
-// // Произошла мутация исходных данных - массива numbers
-// console.log(numbers); // [2, 4, 6, 8, 10]
+// const planetsInUpperCase = planets.map((planet) => planet.toUpperCase());
+// console.log(planetsInUpperCase); // ['ЗЕМЛЯ', 'МАРС', 'ВЕНЕРА', 'ЮПИТЕР']
 
-// Чистая функция (pure function)
+// const planetsInLowerCase = planets.map((planet) => planet.toLowerCase());
+// console.log(planetsInLowerCase); // ['земля', 'марс', 'венера', 'юпитер']
 
-// const pureMultiply = (array, value) => {
-//   const newArray = [];
+// // Оригинальный массив не изменился
+// console.log(planets); // ['Земля', 'Марс', 'Венера', 'Юпитер']
 
-//   array.forEach(element => {
-//     newArray.push(element * value);
-//   });
+const planets = ["Земля", "Марс", "Венера", "Юпитер"];
+// Пиши код ниже этой строки
 
-//   return newArray;
-// };
-
-// const numbers = [1, 2, 3, 4, 5];
-// const doubledNumbers = pureMultiply(numbers, 2);
-
-// // Не произошло мутации исходных данных
-// console.log(numbers); // [1, 2, 3, 4, 5]
-// // Функция вернула новый массив с изменёнными данными
-// console.log(doubledNumbers); // [2, 4, 6, 8, 10]
-
-const changeEven = (numbers, value) => {
-  // Пиши код ниже этой строки
-  // for (let i = 0; i < numbers.length; i += 1) {
-  //   if (numbers[i] % 2 === 0) {
-  //     numbers[i] = numbers[i] + value;
-  //   }
-  // }
-  newArray = [];
-
-  numbers.forEach((element) => {
-    if (element % 2 === 0) {
-      newArray.push(element + value);
-    } else {
-      newArray.push(element);
-    }
-  });
-
-  console.log(newArray);
-
-  return newArray;
-  // Пиши код выше этой строки
-};
-
-changeEven([17, 24, 68, 31, 42], 100);
+const planetsLengths = planets.map((planet) => planet.length);
+console.log(planetsLengths);
